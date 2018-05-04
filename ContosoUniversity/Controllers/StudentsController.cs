0d99d971d615255b9dc3b988707de1ddefd72bb4
alexanderhlee <alexanderhlee@gmail.com>
2018-05-03 20:28:34 -0400

    Beginning of tutorial 3

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -23,11 +23,33 @@ namespace ContosoUniversity.Controllers
         // The return type Task<IActionResult> represents ongoing work (a promise) with a result of type IActionResult.
         // The await keyword causes the compiler to split the method into two parts.The first part ends with the operation that's started asynchronously. The second part is put into a callback method that's called when the operation completes.
         // ToListAsync is the asynchronous version of the ToList extension method.
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            var students = from s in _context.Students select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break;
+            }
+
+
             // Execution of Index() is yielded to the caller here
             // ToListAsync returns a Task<List<Student>>, which is *awaited*
-            return View(await _context.Students.ToListAsync());
+            return View(await students.AsNoTracking().ToListAsync());
         }
 
         // GET: Students/Details/5
